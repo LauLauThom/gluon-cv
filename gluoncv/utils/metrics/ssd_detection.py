8a4146526f48e02ec3dd847fc1be4637d03c6190
Laurent <laurent132.thomas@laposte.net>
2020-03-24 12:33:25 +0100

    use more stuff from mother class

--- gluoncv/utils/metrics/ssd_detection.py
+++ gluoncv/utils/metrics/ssd_detection.py
@@ -1,6 +1,10 @@
-"""SSD Detection Evaluation Metric. """
+"""
+SSD Detection Evaluation Metric. 
+just by defining name, num_instance and sum_metric the inheritance should take care of the rest 
+"""
 
 import mxnet as mx
+from gluoncv.loss import SSDMultiBoxLoss
 
 class SSDDetectionMetric(mx.metric.EvalMetric):
     """
@@ -11,20 +15,20 @@ class SSDDetectionMetric(mx.metric.EvalMetric):
     ssdLoss : gluoncv.loss.SSDMultiBoxLoss
         An instance of SSDMultiBoxLoss
     """
-    def __init__(self, ssdLoss):
+    def __init__(self, ssdLoss=SSDMultiBoxLoss() ):
         super(SSDDetectionMetric, self).__init__('SSDSumLoss')
         self.ssdLoss = ssdLoss
-        
+        self.name = "SumLoss"
         self.reset()
     
     def reset(self):
         """Clear the internal statistics to initial state."""
-        self.sum_loss = 0
+        super(SSDDetectionMetric, self).reset()
         self.cls_loss = 0
         self.box_loss = 0
     
     def update(self, box_preds, cls_preds, scores_preds,
-               box_targets, cls_targets):
+               box_targets, cls_targets, difficult_targets=None):
         """Update internal buffer with latest prediction and  (target) pairs.
 
         Parameters
@@ -41,10 +45,14 @@ class SSDDetectionMetric(mx.metric.EvalMetric):
             Where B is the size of mini-batch, M is the number of ground-truths.
         cls_targets : mxnet.NDArray or numpy.ndarray
             Ground-truth bounding boxes labels with shape `B, M`.
+        difficult_targets : mxnet.NDArray\
+            Difficulty of the instance. Not used but necessary for train_ssd.py
         """
-        self.sum_loss, self.cls_loss, self.box_loss = self.ssdLoss(cls_preds, box_preds, 
+        self.sum_metric, self.cls_loss, self.box_loss = self.ssdLoss(cls_preds, box_preds, 
                                                     cls_targets, box_targets)
-    
+        self.num_inst = len(self.sum_metric[0]) # Batch size (sum_metric = sum(bbox_L1loss + crossentropy) for each image of the batch)
+'''
+# Get will be herited by mother class
     def get(self):
         """Get the current evaluation result.
 
@@ -55,4 +63,5 @@ class SSDDetectionMetric(mx.metric.EvalMetric):
         value : float
            Value of the evaluation.
         """
-        return ("SSDSumLoss", self.sum_loss) 
+        return (self.name, self.sum_loss) 
+'''
